ffac9e857ca03cdcca54f048c19b44f54a303a7d
Mark Fisher <mfisher36@gmail.com>
2018-12-31 15:09:03 -0500

    saving work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 // alert("Hello World!")
 
+window.addEventListener("DOMContentLoaded", function(e) {
+
+ var stage = document.getElementById("stage");
+ var fadeComplete = function(e) { stage.appendChild(arr[0]); };
+ var arr = stage.getElementsByTagName("a");
+ for(var i=0; i < arr.length; i++) {
+   arr[i].addEventListener("animationend", fadeComplete, false);
+ }
+
+}, false);
+
 $(document).ready(function () {
 
 
@@ -55,9 +66,11 @@ $(document).ready(function () {
 
  const $slideshow = $(".slideshow");
  const $gallery = $(".gallery");
+ const $contact = $(".contact");
 
- $slideshow.hide();
- $gallery.show();
+ // $slideshow.hide();
+ // $gallery.hide();
+ // $contact.show();
 
 
  // $slideshow.on("click", function() {
